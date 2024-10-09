758c9a4bf38b4379f268d4025b8836e605a9c9fd
Nicky Huynh <98395576+NguyenNhatHuynh@users.noreply.github.com>
2024-10-09 14:14:38 +0700

    Design UI Home Screen

---
+++ lib/Screens/home.dart
@@ -0,0 +1,103 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Stack(
+          children: [
+            Column(
+              children: [
+                Container(
+                  width: double.infinity,
+                  height: 240,
+                  decoration: BoxDecoration(
+                    color: Color(0xff368983),
+                    borderRadius: BorderRadius.only(
+                      bottomLeft: Radius.circular(20),
+                      bottomRight: Radius.circular(20),
+                    ),
+                  ),
+                  child: Stack(
+                    children: [
+                      Positioned(
+                        top: 5,
+                        left: 340,
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(7),
+                          child: Container(
+                            height: 40,
+                            width: 40,
+                            color: Color.fromRGBO(250, 250, 250, 0.1),
+                            child: Icon(
+                              Icons.notification_add_outlined,
+                              size: 30,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 35, left: 10),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'Chào buổi chiều',
+                              style: TextStyle(
+                                fontWeight: FontWeight.w500,
+                                fontSize: 16,
+                                color: Color.fromARGB(255, 224, 223, 223),
+                              ),
+                            ),
+                            Text(
+                              'Xoan Dev',
+                              style: TextStyle(
+                                fontWeight: FontWeight.w600,
+                                fontSize: 16,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            Positioned(
+              top: 140,
+              left: 37,
+              child: Container(
+                height: 170,
+                width: 320,
+                decoration: BoxDecoration(
+                  color: Color.fromARGB(255, 47, 125, 121),
+                  borderRadius: BorderRadius.circular(15),
+                ),
+                child: Column(children: [
+                  Row(
+                    children: [
+                      Text(
+                        'Tổng Số Dư',
+                        style: TextStyle(
+                          fontWeight: FontWeight.w600,
+                          fontSize: 20,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ],
+                  ),
+                ]),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
