4ad7877947b2834db23fa805804c661859960cee
Nicky Huynh <98395576+NguyenNhatHuynh@users.noreply.github.com>
2024-10-22 08:28:16 +0700

    Fix Bug - No Load Chart - Success

--- lib/data/listdata.dart
+++ lib/data/listdata.dart
@@ -7,23 +7,27 @@ List<money> geter() {
   upwork.time = 'Hôm nay';
   upwork.image = 'Tran.png';
   upwork.buy = false;
+
   money starbucks = money();
   starbucks.buy = true;
   starbucks.free = '15';
-  starbucks.image = 'Star.jpg';
-  starbucks.name = 'starbucks';
+  starbucks.image = 'food.png';
+  starbucks.name = 'Starbucks';
   starbucks.time = 'Hôm nay';
+
   money transportation = money();
   transportation.buy = true;
-  starbucks.free = '150000';
-  starbucks.image = 'Tration.jpg';
-  starbucks.name = 'Đi Grab';
-  starbucks.time = 'Hôm nay';
+  transportation.free = '150000';
+  transportation.image = 'Tration.png';
+  transportation.name = 'Đi Grab';
+  transportation.time = 'Hôm nay';
+
   money trasfer = money();
   trasfer.buy = true;
   trasfer.free = '100';
-  trasfer.image = 'cre.png';
+  trasfer.image = 'Tran.png';
   trasfer.name = 'Chuyển cho Xoan';
-  trasfer.time = 'October 20,2024';
-  return [upwork, starbucks, trasfer, upwork, starbucks, trasfer];
+  trasfer.time = 'October 20, 2024';
+
+  return [upwork, starbucks, transportation, trasfer];
 }
--- lib/widgets/chart.dart
+++ lib/widgets/chart.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:syncfusion_flutter_charts/charts.dart';
 
 class Chart extends StatefulWidget {
@@ -12,37 +11,35 @@ class Chart extends StatefulWidget {
 class _ChartState extends State<Chart> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        width: double.infinity,
-        height: 300,
-        child: SfCartesianChart(
-          primaryXAxis: CategoryAxis(),
-          series: <SplineSeries<SalesData, String>>[
-            SplineSeries<SalesData, String>(
-              color: Color.fromARGB(255, 47, 125, 121),
-              width: 3,
-              dataSource: <SalesData>[
-                SalesData(100, 'Thứ Hai'),
-                SalesData(100, 'Thứ Ba'),
-                SalesData(100, 'Thứ Tư'),
-                SalesData(100, 'Thứ Năm'),
-                SalesData(100, 'Thứ Sau'),
-                SalesData(100, 'Thứ Bảy'),
-                SalesData(100, 'Chủ Nhật'),
-              ],
-              xValueMapper: (SalesData sales, _) => sales.year,
-              yValueMapper: (SalesData sales, _) => sales.sales,
-            )
-          ],
-        ),
+    return Container(
+      width: double.infinity,
+      height: 300,
+      child: SfCartesianChart(
+        primaryXAxis: CategoryAxis(),
+        series: <SplineSeries<SalesData, String>>[
+          SplineSeries<SalesData, String>(
+            color: Color.fromARGB(255, 47, 125, 121),
+            width: 3,
+            dataSource: <SalesData>[
+              SalesData('Thứ.H', 100),
+              SalesData('Thứ.B', 20),
+              SalesData('Thứ.T', 40),
+              //SalesData('Thứ.N', 40),
+              //SalesData('Thứ.S', 50),
+              SalesData('Thứ.B', 15),
+              SalesData('Chủ.N', 5),
+            ],
+            xValueMapper: (SalesData sales, _) => sales.year,
+            yValueMapper: (SalesData sales, _) => sales.sales,
+          )
+        ],
       ),
     );
   }
 }
 
 class SalesData {
-  SalesData(this.sales, this.year);
+  SalesData(this.year, this.sales);
   final String year;
   final int sales;
 }
