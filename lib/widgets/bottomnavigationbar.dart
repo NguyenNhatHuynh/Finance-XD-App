447ef24d62ad639fe38fa63bfc8840cc628b2dad
Nicky Huynh <98395576+NguyenNhatHuynh@users.noreply.github.com>
2024-10-23 08:53:59 +0700

    Design Bottom Navigation Bar

--- lib/widgets/bottomnavigationbar.dart
+++ lib/widgets/bottomnavigationbar.dart
@@ -15,6 +15,51 @@ class _BottomState extends State<Bottom> {
         onPressed: () {},
         child: Icon(Icons.add),
       ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      bottomNavigationBar: BottomAppBar(
+        shape: CircularNotchedRectangle(),
+        child: Padding(
+          padding: const EdgeInsets.only(top: 7.5, bottom: 7.5),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              GestureDetector(
+                onTap: () {},
+                child: Icon(
+                  Icons.home,
+                  size: 30,
+                  color: Color(0xff368983),
+                ),
+              ),
+              SizedBox(width: 20),
+              GestureDetector(
+                onTap: () {},
+                child: Icon(
+                  Icons.bar_chart_outlined,
+                  size: 30,
+                  color: Color(0xff368983),
+                ),
+              ),
+              GestureDetector(
+                onTap: () {},
+                child: Icon(
+                  Icons.account_balance_wallet_outlined,
+                  size: 30,
+                  color: Color(0xff368983),
+                ),
+              ),
+              GestureDetector(
+                onTap: () {},
+                child: Icon(
+                  Icons.person_outline,
+                  size: 30,
+                  color: Color(0xff368983),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
